e4acb59e5d14f1ab0836e1d95b6bcf8d4345419d
RODRIGO FRUTUOSO <fc61865@alunos.fc.ul.pt>
2025-03-16 11:13:42 +0000

    funções no process: fiz todas as funções do process.c, inclui mensagens de erro com exit(1) sempre que haja erros. Vi os slides dos Processos, principalmente a parte II.

--- src/process.c
+++ src/process.c
@@ -6,7 +6,55 @@
 */
 
 #include "../inc/process.h"
+#include "../inc/wallet.h"  // execute_wallet()
+#include "../inc/server.h"  // execute_server()
 
- int launch_wallet(int wallet_id, struct info_container* info, struct buffers* buffs);
- int launch_server(int server_id, struct info_container* info, struct buffers* buffs);
- int wait_process(int process_id);
+#include <stdlib.h>   // exit(), malloc(), free()
+#include <sys/wait.h> // waitpid(), macros WIFEXITED, WEXITSTATUS
+#include <unistd.h>   // fork(), execl(), getpid(), getppid()
+#include <stdio.h>    // printf(), perror()
+
+
+ int launch_wallet(int wallet_id, struct info_container* info, struct buffers* buffs){
+    int pid = fork();
+
+    if (pid == -1) {
+        perror("Error with fork in launch_wallet");
+        exit(1);
+    }
+    if (pid == 0) { 
+        printf("Wallet %d created with PID %d\n", wallet_id, getpid());
+        exit(execute_wallet(wallet_id, info, buffs));
+    }
+    return pid;
+}
+
+int launch_server(int server_id, struct info_container* info, struct buffers* buffs) {
+    int pid = fork();
+
+    if (pid == -1) {
+        perror("Error with fork in launch_server");
+        exit(1);
+    }
+    if (pid == 0) { 
+        printf("Server %d created with PID %d\n", server_id, getpid());
+        exit(execute_server(server_id, info, buffs));
+    }
+    return pid;
+} 
+
+int wait_process(int process_id) {
+    int status;
+    int pid = waitpid(process_id, &status, 0);
+
+    if (pid == -1) {
+        perror("Error with waitpid");
+        exit(1);
+    }
+    if (WIFEXITED(status)) {
+        printf("Process %d terminated with exit code %d\n", pid, WEXITSTATUS(status));
+    } else {
+        printf("Process %d terminated unexpectedly\n", pid);
+    }
+    return pid;
+}
