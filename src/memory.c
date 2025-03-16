a8bd8ba9265ab123e8a24a87f800cbe571b23573
Tiago Leite <tiagobscleite@gmail.com>
2025-03-16 00:17:20 +0000

    4 funções no memory.c
    Implementei as primeiras 4 funções do memory.c, inclui mensagens de erro com exit(1) sempre que haja erros. Vi a ficha da TP05 para me ajudar a perceber estas coisas.

--- src/memory.c
+++ src/memory.c
@@ -6,11 +6,86 @@
 */
 
 #include "../inc/memory.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
-void* allocate_dynamic_memory(int size);
-void* create_shared_memory(char* name, int size);
-void deallocate_dynamic_memory(void* ptr);
-void destroy_shared_memory(char* name, void* ptr, int size);
+void* allocate_dynamic_memory(int size)
+{
+    void *ptr;
+
+    if (size <= 0)
+    {
+        perror("Error with attributes on allocate_dynamic_memory");
+        exit(1);
+    }
+    ptr = calloc(1, size);
+    if (!ptr)
+    {
+        perror("Error with calloc");
+        exit(1);
+    }
+    return (ptr);
+}
+
+void* create_shared_memory(char* name, int size)
+{
+    int fd;
+    void *ptr;
+    char memory_name[256];
+
+    if (!name || size <= 0)
+    {
+        perror("Error with attributes on create_shared_memory");
+        exit(1);
+    }
+    snprintf(memory_name, sizeof(memory_name), "%s_%d", name, getuid());
+    fd = shm_open(memory_name, O_CREAT | O_RDWR, 0666);
+    if (fd == -1)
+    {
+        perror("Error with shm_open");
+        exit(1);
+    }
+    if (ftruncate(fd, size) == -1)
+    {
+        perror("Error with ftruncate");
+        close(fd);
+        shm_unlink(memory_name);
+        exit(1);
+    }
+    ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (ptr == MAP_FAILED)
+    {
+        perror("Error with nmap");
+        close(fd);
+        shm_unlink(memory_name);
+        exit(1);
+    }
+    memset(ptr, 0, size);
+    return (ptr);
+}
+
+void deallocate_dynamic_memory(void* ptr)
+{
+    if (ptr)
+        free(ptr);
+}
+
+void destroy_shared_memory(char* name, void* ptr, int size)
+{
+    if (!name || !ptr || size <= 0)
+    {
+        perror("Error with attributes on destroy_shared_memory");
+        exit(1);
+    }
+    munmap(ptr, size);
+    shm_unlink(name);
+}
+
 void write_main_wallets_buffer(struct ra_buffer* buffer, int buffer_size, struct transaction* tx);
 void write_wallets_servers_buffer(struct circ_buffer* buffer, int buffer_size, struct transaction* tx);
 void write_servers_main_buffer(struct ra_buffer* buffer, int buffer_size, struct transaction* tx);
