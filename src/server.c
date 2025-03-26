8ab75289094bd483ba17325baa2750b7e1bccd19
Tiago Leite <tiagobscleite@gmail.com>
2025-03-26 15:57:56 +0000

    server.c done
    as 4 funções feitas

--- src/server.c
+++ src/server.c
@@ -6,8 +6,71 @@
 */
 
 #include "../inc/server.h"
+#define BUFFER_SIZE 10
 
-int execute_server(int server_id, struct info_container* info, struct buffers* buffs);
-void server_receive_transaction(struct transaction* tx, struct info_container* info, struct buffers* buffs);
-void server_process_transaction(struct transaction* tx, int server_id, struct info_container* info);
-void server_send_transaction(struct transaction* tx, struct info_container* info, struct buffers* buffs);
+int execute_server(int server_id, struct info_container* info, struct buffers* buffs)
+{
+    struct transaction tx;
+    int processed_count = 0;
+
+    while (1)
+    {
+        if (info->terminate)
+            return processed_count;
+        server_receive_transaction(&tx, info, buffs);
+        if (tx.id == -1)
+        {
+            usleep(1000);
+            continue;
+        }
+        server_process_transaction(&tx, server_id, info);
+        server_send_transaction(&tx, info, buffs);
+        processed_count++;
+    }
+}
+
+void server_receive_transaction(struct transaction* tx, struct info_container* info, struct buffers* buffs)
+{
+    if (!tx || !info || !buffs)
+    {
+        perror("Error with attributes on server_receive_transaction");
+        exit(1);
+    }
+
+    if (info->terminate)
+        return;
+
+    read_wallets_servers_buffer(buffs->buff_wallets_servers, BUFFER_SIZE, tx);
+}
+
+void server_process_transaction(struct transaction* tx, int server_id, struct info_container* info)
+{
+    if (!tx || !info || !info->balances || !info->wallets_stats || !info->servers_stats)
+    {
+        perror("Error with attributes on server_process_transaction");
+        exit(1);
+    }
+    if (tx->id == -1 || tx->src_id < 0 || tx->src_id >= info->n_wallets ||
+        tx->dest_id < 0 || tx->dest_id >= info->n_wallets || tx->src_id == tx->dest_id)
+        return;
+    if (info->balances[tx->src_id] < tx->amount)
+        return;
+    if (tx->wallet_signature != info->wallets_stats[tx->src_id])
+        return;
+    info->balances[tx->src_id] -= tx->amount;
+    info->balances[tx->dest_id] += tx->amount;
+    tx->server_signature = server_id;
+    info->servers_stats[server_id]++;
+}
+
+void server_send_transaction(struct transaction* tx, struct info_container* info, struct buffers* buffs)
+{
+    if (!tx || !info || !buffs || !buffs->buff_servers_main)
+    {
+        perror("Error with attributes on server_send_transaction");
+        exit(1);
+    }
+    if (tx->server_signature == -1)
+        return;
+    write_servers_main_buffer(buffs->buff_servers_main, info->buffers_size, tx);
+}
